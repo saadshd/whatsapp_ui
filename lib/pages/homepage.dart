cc07b02f0f7deed19e0c55da571abf755cc133a5
SAADSHD <101456852+SAADSHD@users.noreply.github.com>
2022-09-10 16:14:07 +0500

    Update homepage.dart

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -10,14 +10,54 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('WhatsApp',
-        style: TextStyle(
-          color: Colors.white
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('WhatsApp',
+          style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold
+            ),
+          ),
+          backgroundColor: Colors.teal,
+          bottom: const TabBar(
+              tabs: [
+                Tab(
+                  child: Icon(Icons.camera_alt),
+                ),
+                Tab(
+                  child: Text('CHATS',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Text('STATUS',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Text('CALLS',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+              ]
           ),
         ),
-        backgroundColor: Colors.teal,
+        body: const TabBarView(
+          children: [
+            Text('CAMERA'),
+            Text('CHATS'),
+            Text('STATUS'),
+            Text('CALLS'),
+          ],
+        ),
       ),
     );
   }
